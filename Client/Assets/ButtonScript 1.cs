2b984cad0e3b9520d473997ad6246e8acfbc8cae
Tilde Jensen <tildehojgardjensen@h209.natout.aau.dk>
2016-11-28 16:11:19 +0100

    Menu
    Initial menu

---
+++ Client/Assets/ButtonScript 1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+using System.Collections;
+
+public class ButtonScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void ChangeMenu () {
+		SceneManager.LoadScene ("GameBoard"); 
+
+	}
+
+}

a028874513080615d35a6d883dd3a3562eface29
Tilde Jensen <tildehojgardjensen@h209.natout.aau.dk>
2016-11-30 15:14:00 +0100

    Lobby changes
    Txt changes color when buttons pushed
    + individual texts + color

--- Client/Assets/Scripts/Button2script.cs
+++ Client/Assets/Scripts/Button2script.cs
@@ -5,8 +5,27 @@ using UnityEngine.UI;
 
 public class Button2script : MonoBehaviour {
 
+	Text newText, newText1, newText2;
 
+	void Start() {
 
+		newText = GameObject.Find("Player1B").GetComponentInChildren<Text> ();
+		newText1 = GameObject.Find("Player2B").GetComponentInChildren<Text> ();
+		newText2 = GameObject.Find("Player3B").GetComponentInChildren<Text> ();
+
+	}
+
+	public void changeText() {
+		newText.text = "Something";
+	} 
+
+	public void changeText1() {
+		newText1.text = "Somethingelse";
+	}
+
+	public void changeText2() {
+		newText2.text = "Somethingthird";
+	}
 
 	// Use this for initialization
 	public void Backtomenu () {
@@ -31,12 +50,19 @@ public class Button2script : MonoBehaviour {
 
 	} 
 
+
 	public void changeColor3 ()
 	{
 		Image button1 = GameObject.Find("Player3B").GetComponent<Image>();
-		button1.color = Color.red;
+		button1.color = Color.red; 
 
 	} 
 
 
+
+	public void Awake()
+	{
+
+
+	}
 }

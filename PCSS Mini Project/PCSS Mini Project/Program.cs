6ee4630be01355fdea6debaee6d62f4f92036b61
Lohse54 <lohsea@hotmail.com>
2016-12-04 17:12:49 +0100

    Added Game Running statement
    Added Game Running statement, checking and notifying other players about
    the turn of a new player

--- PCSS Mini Project/PCSS Mini Project/Program.cs
+++ PCSS Mini Project/PCSS Mini Project/Program.cs
@@ -13,33 +13,33 @@ namespace PCSS_Mini_Project
     class TcpEchoClient
     {
         static void Main(string[] args)
-        { 
-			//Variables are being assigned
-			bool activeOr, startActive = true, stillActive = true, isRunning = true;
-			string caseSwitch = "";
-			Console.WriteLine("Starting echo client...");
-			Console.WriteLine("Type 'x' to play");
-			Console.WriteLine("Type 'q' to exit");
+        {
+            //Variables are being assigned
+            bool activeOr, startActive = true, stillActive = true, isRunning = true;
+            string caseSwitch = "";
+            Console.WriteLine("Starting echo client...");
+            Console.WriteLine("Type 'x' to play");
+            Console.WriteLine("Type 'q' to exit");
 
 
-			//While-loop for menu using switch cases
-			while (startActive == true)
-			{
-				caseSwitch = Console.ReadLine();
-				switch(caseSwitch)
-				{
-				case "x":
-					Console.WriteLine("Connecting to Lobby");
-					startActive = false;
-					break;
-				case "q":
-					Environment.Exit(0);
-					break;
-				default:
-					Console.WriteLine("Type 'x' to play, else type 'q' exit");
-					break;
-				}
-			}
+            //While-loop for menu using switch cases
+            while (startActive == true)
+            {
+                caseSwitch = Console.ReadLine();
+                switch (caseSwitch)
+                {
+                    case "x":
+                        Console.WriteLine("Connecting to Lobby");
+                        startActive = false;
+                        break;
+                    case "q":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Type 'x' to play, else type 'q' exit");
+                        break;
+                }
+            }
 
             //Connects to the server
             string playerReceive, playerTurn, tempString, placement;
@@ -61,6 +61,58 @@ namespace PCSS_Mini_Project
 
             Random rnd = new Random();
 
+
+
+
+            //Game is running until it exits loop
+            while (isRunning)
+            {
+                writer.WriteLine("");
+                playerTurn = reader.ReadLine();
+                //Searching for winning statements
+                if (playerTurn == "Player1 Won")
+                {
+                    Console.WriteLine(playerTurn);
+                    isRunning = false;
+                }
+                if (playerTurn == "Player2 Won")
+                {
+                    Console.WriteLine(playerTurn);
+                    isRunning = false;
+                }
+                if (playerTurn == "Player3 Won")
+                {
+                    Console.WriteLine(playerTurn);
+                    isRunning = false;
+                }
+                //If not the player, send the playername of the one that has the turn
+                if (playerReceive != playerTurn)
+                {
+                    Console.WriteLine(playerTurn + " has the turn this round");
+                    while (playerReceive != playerTurn)
+                    {
+                        writer.WriteLine("");
+                        tempString = reader.ReadLine();
+                        //Check again for winning statement
+                        if (tempString == "Player1 Won")
+                        {
+                            Console.WriteLine(tempString);
+                            isRunning = false;
+                        }
+                        if (tempString == "Player2 Won")
+                        {
+                            Console.WriteLine(tempString);
+                            isRunning = false;
+                        }
+                        if (tempString == "Player3 Won")
+                        {
+                            Console.WriteLine(tempString);
+                            isRunning = false;
+                        }
+                        //Check again for if not the current player and get response from server with the current player
+                    }
+                }
+            }
         }
     }
 }

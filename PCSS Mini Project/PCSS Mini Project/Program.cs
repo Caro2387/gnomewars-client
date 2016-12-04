90bc6d7691fd3ecddfad69cf3a140b78bd0e9fd9
Tilde Jensen <tildehojgardjensen@Melvin.local>
2016-12-04 16:59:02 +0100

    Startup for Client
    Added variables, and a While-loop for menu using switch cases.

--- PCSS Mini Project/PCSS Mini Project/Program.cs
+++ PCSS Mini Project/PCSS Mini Project/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
+using System.IO; 
+using System.Net; 
+using System.Net.Sockets;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PCSS_Mini_Project
 {
-    class Program
+    class TcpEchoClient
     {
         static void Main(string[] args)
-        {
+        { 
+			//Variables are being assigned
+			bool activeOr, startActive = true, stillActive = true, isRunning = true;
+			string caseSwitch = "";
+			Console.WriteLine("Starting echo client...");
+			Console.WriteLine("Type 'x' to play");
+			Console.WriteLine("Type 'q' to exit");
+
+
+			//While-loop for menu using switch cases
+			while (startActive == true)
+			{
+				caseSwitch = Console.ReadLine();
+				switch(caseSwitch)
+				{
+				case "x":
+					Console.WriteLine("Connecting to Lobby");
+					startActive = false;
+					break;
+				case "q":
+					Environment.Exit(0);
+					break;
+				default:
+					Console.WriteLine("Type 'x' to play, else type 'q' exit");
+					break;
+				}
+			}
+		//Insert Here!!! 
+
         }
     }
 }
